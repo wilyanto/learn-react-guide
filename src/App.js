094bbf8ea70a46672e86dbb3de8eddf80d88f819
wilyanto <wilyanto@code.id>
2021-10-01 21:48:56 +0700

    Added NewExpense Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Hello there</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
