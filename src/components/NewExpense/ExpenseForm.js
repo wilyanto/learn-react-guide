08497a1e84fa87a14cae0775ae0e01c1ca752f09
wilyanto <wilyanto@code.id>
2021-10-01 22:40:35 +0700

    using 1 state instead of many useState

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,18 +2,35 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput((p) => {
+      return {
+        ...p,
+        enteredTitle: event.target.value,
+      };
+    });
   };
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    setUserInput((p) => {
+      return {
+        ...p,
+        enteredAmount: event.target.value,
+      };
+    });
   };
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    setUserInput((p) => {
+      return {
+        ...p,
+        enteredDate: event.target.value,
+      };
+    });
   };
 
   return (
